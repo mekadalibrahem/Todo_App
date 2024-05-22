41643ae13896f710c1f0d02222725f71163f2686
MekadAlIibrahem <mekadalibrahem123@gmail.com>
2024-05-22 13:06:10 +0300

    init resources

---
+++ app/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    alias(libs.plugins.com.android.application)
+}
+
+android {
+    namespace = "com.example.todo_app"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.todo_app"
+        minSdk = 28
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    val work_version  = "2.9.0"
+    // (Java only)
+    implementation("androidx.work:work-runtime:$work_version")
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
