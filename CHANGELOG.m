285402a62cff4a0b6d3a06190cbbec9126f896c2
Mekad AlIibrahem <79482795+mekadalibrahem@users.noreply.github.com>
2024-05-22 16:41:18 +0300

    Create CHANGELOG.m

---
+++ CHANGELOG.m
@@ -0,0 +1,6 @@
+#v1.0 
+
+-  categorey managment ( add , edit and delete ) options
+-  task managemt ( add , edit , and delete ) options
+-  reminder managemnt ( add and delete ) options
+-  show notification for each reminders data time
